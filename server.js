5bb3b92738b1bdb7c9b9dba7fd20ea5a7e23e709
arab11111 <arabmessara@gmail.com>
2026-05-07 16:39:45 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- server.js
+++ server.js
@@ -533,7 +533,7 @@ io.on('connection', async (socket) => {
 
 // Start
 const PORT = process.env.PORT || 3002;
-server.listen(PORT, () => console.log('Server started on', PORT));
+
 server.listen(PORT, '0.0.0.0', () => {
   console.log('Server started on', PORT);
 });
